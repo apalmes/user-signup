ff077b1bb431161d74813d86491be4559b3ef641
asclepiusgal <jeanna.e.clark@gmail.com>
2017-06-21 20:27:29 -0700

    code review

--- main.py
+++ main.py
@@ -1,71 +1,63 @@
+#!/usr/bin/env python
+
+__author__ = "student"
+__version__ = "1.0"
+# June 2017
+# Flask User Sign-up re: LaunchCode
+# Rubric: http://education.launchcode.org/web-fundamentals/assignments/user-signup/
+
+
 from flask import Flask, redirect, request, render_template
-import cgi
+import re
 
 app = Flask(__name__)
-
 app.config['DEBUG'] = True 
 
-def user():
-    user=username = request.form['username']
 
-@app.route('/sign-up', methods=['POST'])
+@app.route('/', methods=['POST', 'GET'])
 def user_signup():
-    
 
-    username = request.form['username']
-    password = request.form['password']
-    verify = request.form['verify']
-    email = request.form['email']
-
+    username = ''
+    email = ''
     user_error = ''
-    password_error= ''
-    verify_error=''
-    email_error=''
+    password_error = ''
+    verify_error = ''
+    email_error = ''
+    title = 'User Sign-up'
 
-    if (len(username) < 3) or (len(username) > 20) or username == "":
-        user_error = "That is not a valid username"
-        
-    for char in username:
-        if char == " ":
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+        email = request.form['email']
+
+        if (len(username) < 3) or (len(username) > 20) or (username == "") or (username.count(" ") > 0):
             user_error = "That is not a valid username"
-            
-    if (len(password) < 3) or (len(password) > 20) or password == "":
-        password_error = "That is not a valid password"
+            username = ''
+
+        if (len(password) < 3) or (len(password) > 20) or (password == "") or (password.count(" ") > 0):
+            password_error = "That is not a valid password"
+
+        if (verify != password) or (password == ""):
+            verify_error = "The passwords do not match"
+
+        if (email != '') and (not re.match('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email)):
+            email_error = "That is not a valid email"
+            email = ''
     
-    for char in password:
-        if char == " ":
-            password_error = 'That is not a valid password'
-    
-    if (verify != password) or password == '':
-        verify_error="The passwords do not match"
+        if (not user_error) and (not password_error) and (not verify_error) and (not email_error):
+            return redirect('/welcome?username={0}'.format(username))
+
+    return render_template('sign-up.html', title=title, username=username, email=email, user_error=user_error,
+                           password_error=password_error, verify_error=verify_error, email_error=email_error)
 
 
-    if (len(email) < 3)  or (len(email) > 20) and '@' or "." not in email:
-        email_error = "That is not a valid email"
-    
-    if email == "":
-        email_error= ""
-
-    for char in email:
-        if char == " ":
-            email_error= 'That is not a valid email'
-    
-    if not user_error and not password_error and not verify_error and not email_error:
-        return redirect('/welcome?username={0}'.format(username))
-
-    else:
-        return render_template('sign-up.html', title='User Sign-up', user_error=user_error, password_error=password_error, verify_error=verify_error, email_error=email_error)
-
-@app.route('/welcome', methods=['GET','POST'])
-
+@app.route('/welcome')
 def welcome():
-    username=request.args.get('username')
-    
-    return render_template('welcome.html', title='Welcome!', username=username)
+    username = request.args.get('username')
+    title = 'Welcome!'
+    return render_template('welcome.html', title=title, username=username)
 
-@app.route('/')
-def index():
-    
-    return render_template('sign-up.html', title='User Sign-up')
 
-app.run()
+if __name__ == '__main__':
+    app.run()
